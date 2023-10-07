9325e680272150ed7afdf7d7019675930ec341ac
石井 元康 <regenwurm770@gmail.com>
2023-10-07 23:43:31 +0900

    ブロックを7までつくった

---
+++ Assets/Block7Info.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block7Info : BlockInfo
+{
+    public override void SetSelfPrefab()
+    {
+        selfPrefab = (GameObject)Resources.Load("SevenBlock");
+    }
+
+    public override void AddRigidbody2D()
+    {
+        Rigidbody2D rb2D = gameObject.AddComponent<Rigidbody2D>();
+        rb2D.freezeRotation = true;
+    }
+    public override void SetMyNumber()
+    {
+        myNumber = 7;
+    }
+}
