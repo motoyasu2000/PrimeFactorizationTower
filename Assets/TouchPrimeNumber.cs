165ab314dd17d90de6ae861223a37d8204965d72
石井 元康 <regenwurm770@gmail.com>
2023-10-09 21:05:15 +0900

    UIだけ作った。あとは生成。あとボタンを押したときにも空中のblockも反応してしまっていたのでそれを修正した。

--- Assets/TouchPrimeNumber.cs
+++ Assets/TouchPrimeNumber.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TouchPrimeNumber : MonoBehaviour
 {
@@ -24,44 +25,48 @@ public class TouchPrimeNumber : MonoBehaviour
         //Input.touchesはフレームごとのtouchの数を取得する(指の本数や、１フレームでの超高速touch)
         foreach (Touch touch in Input.touches)
         {
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position); //ワールド座標に変換
-            touchPosition.z = Camera.main.nearClipPlane; //クリッピングされないように
-
-            // タッチの状態に応じて処理
-            switch (touch.phase)
+            if (touch.fingerId == 0)
             {
-                //タッチした瞬間であれば
-                case TouchPhase.Began:
-                    if (!isDragging)
-                    {
-                        if (singleGenerateManager.GetSingleGameObject() == null) return;
-                        draggedObject = singleGenerateManager.GetSingleGameObject().transform;
-                        draggedObject.position = new Vector3(touchPosition.x, primeNumberGeneratingPoint.transform.position.y, touchPosition.z); //ブロックx座標をタッチしている座標に
-                        isDragging = true; //ドラッグ状態にする (もしかするといらないかも)
-                    }
-                    break;
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position); //ワールド座標に変換
+                touchPosition.z = Camera.main.nearClipPlane; //クリッピングされないように
 
-                //タッチしている間であれば
-                case TouchPhase.Moved:
-                    if (isDragging) //もしドラッグ中なら
-                    {
-                        draggedObject.position = new Vector3(touchPosition.x, primeNumberGeneratingPoint.transform.position.y, touchPosition.z); //ブロックx座標をタッチしている座標に
-                    }
-                    break;
+                // タッチの状態に応じて処理
+                switch (touch.phase)
+                {
+                    //タッチした瞬間であれば
+                    case TouchPhase.Began:
+                        if (!isDragging)
+                        {
+                            if (EventSystem.current.IsPointerOverGameObject(0)) return;
+                            if (singleGenerateManager.GetSingleGameObject() == null) return;
+                            draggedObject = singleGenerateManager.GetSingleGameObject().transform;
+                            draggedObject.position = new Vector3(touchPosition.x, primeNumberGeneratingPoint.transform.position.y, touchPosition.z); //ブロックx座標をタッチしている座標に
+                            isDragging = true; //ドラッグ状態にする (もしかするといらないかも)
+                        }
+                        break;
 
-                //タッチを終わらせたなら(指を離す)
-                case TouchPhase.Ended:
-                    if (isDragging)
-                    {
-                        isDragging = false; //ドラッグ状態を解除
-                        draggedObject = null; //何のオブジェクトも洗濯していない状態に
-                        singleGenerateManager.SetSingleGameObject(null); //このブロックがsingleGameObjectに入ったままにしていると、ボタンが押された瞬間にDestroyが呼ばれてしまう。
-                        this.enabled = false; //このスクリプトのｉｎｓｔａｎｃｅを消去し、タッチできないようにする。
-                        this.tag = "PrimeNumberBlock"; //タグを素数オブジェクトに変更する
-                        gameObject.layer = LayerMask.NameToLayer("PrimeNumberBlock"); //レイヤーも素数ブロックにかえる
-                        blockInfo.AddRigidbody2D(); //重力を加える。
-                    }
-                    break;
+                    //タッチしている間であれば
+                    case TouchPhase.Moved:
+                        if (isDragging) //もしドラッグ中なら
+                        {
+                            draggedObject.position = new Vector3(touchPosition.x, primeNumberGeneratingPoint.transform.position.y, touchPosition.z); //ブロックx座標をタッチしている座標に
+                        }
+                        break;
+
+                    //タッチを終わらせたなら(指を離す)
+                    case TouchPhase.Ended:
+                        if (isDragging)
+                        {
+                            isDragging = false; //ドラッグ状態を解除
+                            draggedObject = null; //何のオブジェクトも洗濯していない状態に
+                            singleGenerateManager.SetSingleGameObject(null); //このブロックがsingleGameObjectに入ったままにしていると、ボタンが押された瞬間にDestroyが呼ばれてしまう。
+                            this.enabled = false; //このスクリプトのｉｎｓｔａｎｃｅを消去し、タッチできないようにする。
+                            this.tag = "PrimeNumberBlock"; //タグを素数オブジェクトに変更する
+                            gameObject.layer = LayerMask.NameToLayer("PrimeNumberBlock"); //レイヤーも素数ブロックにかえる
+                            blockInfo.AddRigidbody2D(); //重力を加える。
+                        }
+                        break;
+                }
             }
         }
     }
