c0d0d1f320a32792bda9bd03154fe89a32eede90
motoyasu2000 <regenwurm770@gmail.com>
2023-12-13 00:26:04 +0900

    遅れて文字が表示されるようにした。かっこいい

--- Assets/MainTextManager.cs
+++ Assets/MainTextManager.cs
@@ -18,6 +18,15 @@ public class MainTextManager : MonoBehaviour
         StartCoroutine(HiddenMainText());
     }
 
+    public IEnumerator TmpPrintMainText(string str, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        mainText.gameObject.SetActive(true);
+        mainText.text = str;
+        StartCoroutine(HiddenMainText());
+        yield return null;
+    }
+
     IEnumerator HiddenMainText()
     {
         yield return new WaitForSeconds(1.2f);
