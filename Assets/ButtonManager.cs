9761cd2e91688a9ff3640c524716be0b7f8b5cc1
motoyasu2000 <regenwurm770@gmail.com>
2023-12-19 03:09:29 +0900

    SinglePlayのメニューを作った。最初のメニューとsingleplayのメニューで推移できるようにした。

---
+++ Assets/ButtonManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] GameObject singlePlay;
+    [SerializeField] GameObject multiPlay;
+    [SerializeField] GameObject setting;
+    [SerializeField] GameObject credit;
+    GameObject[] menus = new GameObject[4];
+
+    void Awake()
+    {
+        singlePlay = GameObject.Find("SinglePlay");
+        multiPlay = GameObject.Find("MultiPlay");
+        setting = GameObject.Find("Setting");
+        credit = GameObject.Find("Credit");
+
+        menus[0] = singlePlay;
+        menus[1] = multiPlay;
+        menus[2] = setting;
+        menus[3] = credit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayMenu(GameObject menu)
+    {
+        menu.SetActive(true);
+    }
+
+    public void BackFirstMenu()
+    {
+        foreach (var menu in menus)
+        {
+            if(menu==null) continue;
+            menu.gameObject.SetActive(false);
+        }
+    }
+}
