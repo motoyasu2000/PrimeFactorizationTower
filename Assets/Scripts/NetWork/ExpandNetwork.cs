23eb1da0621eb3deade8c98fee12957f9ef8f81e
motoyasu2000 <regenwurm770@gmail.com>
2023-12-22 00:29:57 +0900

    closedlistの更新がうまくいっていなかったので修正した。次は条件が変わる際にネットワーク内にその条件にマッチするサブグラフはないかを探すようにした方がいいはずなので、その修正を行う

--- Assets/Scripts/NetWork/ExpandNetwork.cs
+++ Assets/Scripts/NetWork/ExpandNetwork.cs
@@ -43,8 +43,9 @@ public class ExpandNetwork
                 backFlag = true;
             }
         }
-        //Debug.Log(string.Join(", ", closedList));
-        //Debug.Log(string.Join(", ", myNetwork));
+        closedList.Add(nowNode);
+        Debug.Log(string.Join(", ", closedList));
+        Debug.Log(string.Join(", ", myNetwork));
     }
 
     // ネットワークに隣接ノードを追加するメソッド（重複を防ぐ）
