9894136021cf70f9687cd6a0c6db932cd390204f
motoyasu2000 <regenwurm770@gmail.com>
2023-12-23 00:40:44 +0900

    7の形を30度と150度のひし形にした。これによって3の正三角形にフィットするようになった。また、3が大きすぎたので小さくした。

--- Assets/SingletonGenerator.cs
+++ Assets/SingletonGenerator.cs
@@ -6,6 +6,7 @@ public class SingletonGenerator : MonoBehaviour
 {
     private void Awake()
     {
+        //もし、ゲームモードマネージャーのstaticインスタンスが存在しなければ、ゲームモードマネージャーがアタッチされたインスタンスを生成し、初期化を行う。
         if (GameModeManager.GameModemanagerInstance == null)
         {
             GameObject gameModeManager = new GameObject("GameModeManager");
