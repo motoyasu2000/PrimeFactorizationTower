4f6ef76fd01529b61289cf58bbca17078e8f2012
motoyasu2000 <regenwurm770@gmail.com>
2023-12-28 18:27:28 +0900

    singletonパターンの保存したいフィールドの変数の就職しをシリアライズ可能にするためにpublicにしていたが、カプセル化のために[serializefield]を使うように変更した。

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -11,9 +11,14 @@ public class SoundManager : MonoBehaviour
     private static SoundManager instance;
     public static SoundManager SoundManagerInstance => instance;
 
-    public float volume_BGM;
-    public float volume_SE;
-    public float volume_Voice;
+    //保存を行う変数たち、スライダーの値によって変更する
+    [SerializeField] float volume_BGM;
+    [SerializeField] float volume_SE;
+    [SerializeField] float volume_Voice;
+
+    public float Volume_BGM => volume_BGM;
+    public float Volume_SE => volume_SE;
+    public float Volume_Voice => volume_Voice;
 
     Transform transVoices;
     Transform transSEs;
@@ -64,6 +69,19 @@ public class SoundManager : MonoBehaviour
     private void Update()
     {
         instance.SoundSetting();
+    }
+
+    public void SetVolumeBGM(float newVolume)
+    {
+        volume_BGM = newVolume;
+    }
+    public void SetVolumeSE(float newVolume)
+    {
+        volume_SE = newVolume;
+    }
+    public void SetVolumeVoice(float newVolume)
+    {
+        volume_Voice = newVolume;
     }
     public void PlayAudio(AudioSource audioSource)
     {
--- Assets/VolumeSetter.cs
+++ Assets/VolumeSetter.cs
@@ -13,9 +13,9 @@ public class VolumeSetter : MonoBehaviour
         sliders[1] = GameObject.Find("SE_Volume").transform.GetChild(0).GetComponent<Slider>();
         sliders[2] = GameObject.Find("Voice_Volume").transform.GetChild(0).GetComponent<Slider>();
 
-        if (sliders[0] != null) sliders[0].value = SoundManager.SoundManagerInstance.volume_BGM;
-        if (sliders[0] != null) sliders[1].value = SoundManager.SoundManagerInstance.volume_SE;
-        if (sliders[0] != null) sliders[2].value = SoundManager.SoundManagerInstance.volume_Voice;
+        if (sliders[0] != null) sliders[0].value = SoundManager.SoundManagerInstance.Volume_BGM;
+        if (sliders[0] != null) sliders[1].value = SoundManager.SoundManagerInstance.Volume_SE;
+        if (sliders[0] != null) sliders[2].value = SoundManager.SoundManagerInstance.Volume_Voice;
 
         Debug.Log(sliders[0].value + " " +sliders[1].value + " " + sliders[2].value);
     }
@@ -27,23 +27,23 @@ public class VolumeSetter : MonoBehaviour
     }
     public void SetVolumeBGM()
     {
-        preSliderValue[0] = sliders[0].value;
-        SoundManager.SoundManagerInstance.volume_BGM = sliders[0].value;
+        SoundManager.SoundManagerInstance.SetVolumeBGM(sliders[0].value);
         Debug.Log(gameObject);
         if (preSliderValue[0] != sliders[0].value) SoundManager.SaveSoundData(); //スライダーの値が変更されたらセーブを行う
+        preSliderValue[0] = sliders[0].value;
     }
     public void SetVolumeSE()
     {
-        preSliderValue[0] = sliders[0].value;
-        SoundManager.SoundManagerInstance.volume_SE = sliders[1].value;
+        SoundManager.SoundManagerInstance.SetVolumeSE(sliders[1].value);
         Debug.Log(gameObject);
         if (preSliderValue[1] != sliders[1].value) SoundManager.SaveSoundData(); //スライダーの値が変更されたらセーブを行う
+        preSliderValue[1] = sliders[1].value;
     }
     public void SetVolumeVoice()
     {
-        preSliderValue[0] = sliders[0].value;
-        SoundManager.SoundManagerInstance.volume_Voice = sliders[2].value;
+        SoundManager.SoundManagerInstance.SetVolumeVoice(sliders[2].value);
         Debug.Log(gameObject);
         if (preSliderValue[2] != sliders[2].value) SoundManager.SaveSoundData(); //スライダーの値が変更されたらセーブを行う
+        preSliderValue[2] = sliders[2].value;
     }
 }
