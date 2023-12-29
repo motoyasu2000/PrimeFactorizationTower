4d89e6ef1d635a7545c76d51783e82552a54dd62
motoyasu2000 <regenwurm770@gmail.com>
2023-12-30 02:23:02 +0900

    ランキングを正常に取得し、表示する処理を追加した。

--- Assets/ButtonManager.cs
+++ Assets/ButtonManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,8 +12,11 @@ public class ButtonManager : MonoBehaviour
     [SerializeField] GameObject history;
     [SerializeField] GameObject setting;
     [SerializeField] GameObject credit;
+    Transform ranking_transform;
     Button[] difficultyLevelButtons = new Button[3];
     GameObject[] menus = new GameObject[5];
+    
+
 
     [SerializeField] Scene playScene;
 
@@ -25,6 +29,7 @@ public class ButtonManager : MonoBehaviour
         credit = GameObject.Find("Credit");
 
         InitializeDifficultyLevelButton();
+        InitializeHistory();
 
         menus[0] = singlePlay;
         menus[1] = multiPlay;
@@ -88,12 +93,36 @@ public class ButtonManager : MonoBehaviour
         button.GetComponent<Image>().color = new Color(150f / 255f, 150f / 255f, 150f / 255f, 1);
     }
 
+    //最初にどの難易度ボタンが光っているか
     void InitializeDifficultyLevelButton()
     {
-        if (setting == null) return; //設定画面でない場合、処理を行わない。
+        if (setting == null && history ==null) return; //設定画面でない場合、処理を行わない。
         difficultyLevelButtons[0] = GameObject.Find("NormalButton").GetComponent<Button>();
         difficultyLevelButtons[1] = GameObject.Find("DifficultButton").GetComponent<Button>();
         difficultyLevelButtons[2] = GameObject.Find("InsaneButton").GetComponent<Button>();
         ChangeDifficultyLevel((int)GameModeManager.GameModemanagerInstance.NowDifficultyLevel);
+    }
+
+    void InitializeHistory()
+    {
+        int diffLevel = (int)GameModeManager.GameModemanagerInstance.NowDifficultyLevel;
+        int gameMode = (int)GameModeManager.GameModemanagerInstance.NowGameMode;
+        DisplayRankingScores(diffLevel);
+    }
+    
+    public void DisplayRankingScores(int diffLevel)
+    {
+        if (history == null) return;
+        ranking_transform = history.transform.Find("Ranking");
+        int rankCounter = 0;
+        //※他のゲームモードが追加されたならこのあたりに条件分岐で今のゲームモードのスコアのものを取得する処理を書く※
+        int[] scores;
+        scores = ScoreManager.ScoreManagerInstance.PileUpScores[(GameModeManager.DifficultyLevel)diffLevel];
+        foreach (Transform sell in ranking_transform)
+        {
+            if (sell.name.Substring(0, 4) != "Sell") return;
+            TextMeshProUGUI rank = sell.transform.Find("Score").GetComponent<TextMeshProUGUI>();
+            rank.text = scores[rankCounter++].ToString();
+        }
     }
 }
