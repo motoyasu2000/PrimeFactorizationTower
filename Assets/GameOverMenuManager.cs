b9bbf7de1d1ee15a318f9265a7bcb26d00a80288
motoyasu2000 <regenwurm770@gmail.com>
2023-12-29 18:27:48 +0900

    適切にスコアが表示されるようにした。

--- Assets/GameOverMenuManager.cs
+++ Assets/GameOverMenuManager.cs
@@ -21,6 +21,7 @@ public class GameOverMenuManager : MonoBehaviour
         gameModeManager = GameModeManager.GameModemanagerInstance;
 
         PrintGameOverReason();
+        DisplayScoreMenu();
     }
 
     void Update()
@@ -35,5 +36,39 @@ public class GameOverMenuManager : MonoBehaviour
             if (gameManager.PrimeNumber_GO != 0) gameOverReason.text = $"{gameManager.CompositeNumber_GO} / {gameManager.PrimeNumber_GO} = {gameManager.CompositeNumber_GO / gameManager.PrimeNumber_GO}...{gameManager.CompositeNumber_GO % gameManager.PrimeNumber_GO}";
             else gameOverReason.text = "Fell Down";
         }
+    }
+
+    void DisplayScoreMenu()
+    {
+        oldMaxScore = gameManager.OldMaxScore;
+        newScore = gameManager.NewScore;
+        Debug.Log(oldMaxScore);
+        if (newScore <= oldMaxScore)
+        {
+            Scores_NonUpdateRecord.SetActive(true);
+            PrintNonUpdateRecord();
+        }
+        else
+        {
+            Scores_UpdateRecord.SetActive(true);
+            PrintUpdateRecord();
+        }
+        
+    }
+
+    void PrintNonUpdateRecord()
+    {
+        TextMeshProUGUI newScoreText = Scores_NonUpdateRecord.transform.Find("NewScore").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI oldScoreText = Scores_NonUpdateRecord.transform.Find("OldScore").GetComponent<TextMeshProUGUI>();
+        newScoreText.text = "New Score: " + newScore.ToString();
+        oldScoreText.text = "Max Score: " +oldMaxScore.ToString();
+    }
+
+    void PrintUpdateRecord()
+    {
+        TextMeshProUGUI newScoreText = Scores_UpdateRecord.transform.Find("NewScore").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI oldScoreText = Scores_UpdateRecord.transform.Find("OldScore").GetComponent<TextMeshProUGUI>();
+        newScoreText.text = newScore.ToString();
+        oldScoreText.text = "Old Max: " + oldMaxScore.ToString();
     }
 }
