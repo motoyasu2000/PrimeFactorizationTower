5cd6e01052a4700300dfec0e3816f0ca30af04e4
motoyasu2000 <regenwurm770@gmail.com>
2024-02-28 01:03:50 +0900

    一旦はeffect用のシーンを作り、effectを表示させることができた。

---
+++ Assets/Scripts/Appearance/NOT_UI/TapEffect/TapEffectSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapEffectSpawner : MonoBehaviour
+{
+    GameObject tapEffect;
+
+    private void Start()
+    {
+        tapEffect = Resources.Load("TapEffect") as GameObject;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0); //最初のタッチを取得
+
+            //タッチが開始された瞬間にエフェクトを生成
+            if (touch.phase == TouchPhase.Began)
+            {
+                //スクリーン座標をワールド座標に変換
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                touchPosition.z = 0;
+
+                //エフェクトをインスタンス化
+                Instantiate(tapEffect, touchPosition, Quaternion.identity);
+            }
+        }
+    }
+}
