1be6f44573bd6f34d3344ae31522de4d91d02bc7
motoyasu2000 <regenwurm770@gmail.com>
2024-02-29 01:49:12 +0900

    正常にUIの前面にeffectが表示されるようにした。この変更に伴い、CanvasのRenderModeをScreenSpace_Cameraにしたことにより、UIのサイズを調整してたスクリプトの内容を変える必要がある。

--- Assets/Scripts/Appearance/NOT_UI/TapEffect/TapEffectSpawner.cs
+++ Assets/Scripts/Appearance/NOT_UI/TapEffect/TapEffectSpawner.cs
@@ -8,6 +8,8 @@ public class TapEffectSpawner : MonoBehaviour
     GameObject tapEffect;
     SoundManager soundManager;
     Camera tapEffectCamera;
+    Canvas canvas;
+    Touch touch;
     Scene tapEffectScene;
 
     private void Start()
@@ -15,6 +17,7 @@ public class TapEffectSpawner : MonoBehaviour
         tapEffect = Resources.Load("TapEffect") as GameObject;
         soundManager = SoundManager.SoundManagerInstance;
         tapEffectCamera = GameObject.Find("TapEffectCamera").GetComponent<Camera>();
+        canvas = GameObject.Find("Canvas").GetComponent <Canvas>();
         tapEffectScene = SceneManager.GetSceneByName("TapEffectScene");
     }
 
@@ -22,21 +25,41 @@ public class TapEffectSpawner : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
-            Touch touch = Input.GetTouch(0); //最初のタッチを取得
+            touch = Input.GetTouch(0); //最初のタッチを取得
 
             //タッチが開始された瞬間にエフェクトを生成
             if (touch.phase == TouchPhase.Began)
             {
-                //スクリーン座標をワールド座標に変換
-                Vector3 touchPosition = tapEffectCamera.ScreenToWorldPoint(touch.position);
-                touchPosition.z = 0;
-
-                //エフェクトをインスタンス化して、TapEffectManager内に送る
-                GameObject nowTapEffect = Instantiate(tapEffect, touchPosition, Quaternion.identity);
-                SceneManager.MoveGameObjectToScene(nowTapEffect, tapEffectScene);
-
+                SpawnEffect();
+            }
+            //指が画面にずっとついている状態であり、動いていないときエフェクトを生成
+            if (touch.phase == TouchPhase.Stationary)
+            {
+                SpawnEffect();
+            }
+            //指が画面にずっとついている状態であり、動いているときエフェクトを生成
+            if (touch.phase == TouchPhase.Stationary)
+            {
+                SpawnEffect();
+            }
+            //指を話したとき、SEを再生
+            if(touch.phase == TouchPhase.Ended)
+            {
                 soundManager.PlayAudio(soundManager.SE_TAP);
             }
         }
+    }
+
+    void SpawnEffect()
+    {
+        //スクリーン座標をワールド座標に変換
+        Vector3 touchPosition = tapEffectCamera.ScreenToWorldPoint(touch.position);
+        touchPosition.z = 0;
+
+        //エフェクトをインスタンス化して、TapEffectScen内に送る
+        GameObject nowTapEffect = Instantiate(tapEffect, touchPosition, Quaternion.identity);
+        SceneManager.MoveGameObjectToScene(nowTapEffect, tapEffectScene);
+
+        
     }
 }
