65cdd57b3235104ff2a47d99ee3cb30496365d6e
motoyasu2000 <regenwurm770@gmail.com>
2024-03-19 02:15:35 +0900

    データベースの設計が異常であることが判明したため、修正を行った。今回のゲームの仕様では、IDとModeと難易度に対してスコアは一つであってほしいため、今回の設計はダメダメだった。IDがハッシュキー、Modeと難易度がソートキーのデータベースと、Modeと難易度がハッシュキー、スコアがソートキーのデータベースの2つが考えられるが、Modeと難易度ごとのランキングを表示させるコストを考えると、後者が適切であると考えたため、後者に変更した。

--- Assets/Scripts/AWS/DynamoDB/DynamoDBManager.cs
+++ Assets/Scripts/AWS/DynamoDB/DynamoDBManager.cs
@@ -41,7 +41,6 @@ namespace AWS
         //スコアをDynamoDBに非同期で保存する。
         public async Task SaveScoreAsync(string modeAndLevel, int newScore)
         {
-            string playerIDAndModeAndLevel = $"{playerID}_{modeAndLevel}";
             try
             {
                 int oldScore = 0;
@@ -51,8 +50,9 @@ namespace AWS
                 {
                     var ranking = new DynamoDBDatas
                     {
-                        PlayerIDAndModeAndLevel = playerIDAndModeAndLevel,
+                        ModeAndLevel = modeAndLevel,
                         Score = newScore,
+                        PlayerID = playerID,
                     };
 
                     //DynamoDBにスコアを保存、なぜ失敗したのかなどの情報がresultにcallbackされる
@@ -61,7 +61,6 @@ namespace AWS
                         if (result.Exception == null)
                         {
                             Debug.Log("スコア更新成功！");
-                            DeleteOldScoreAsync(playerIDAndModeAndLevel, oldScore); //古いスコアを消去
                         }
                         else
                         {
@@ -86,14 +85,11 @@ namespace AWS
         //引数で指定されたモードに対するrecordをDynamoDBから非同期で取得する。(Task型で戻す)
         public void GetRecordAsync(string modeAndLevel, Action<DisplayScores> callback)
         {
-            string playerIDAndModeAndLevel = $"{playerID}_{modeAndLevel}";
             try
             {
-                //QueryAsyncを使用する際にはクエリの情報を持つクラスQueryRequestのインスタンスが必要で、ここでクエリの細かい設定を行う。
-                var highestScoreQuery = GetRecordsQuery(playerIDAndModeAndLevel, false, 1);
-
+                var singleQuery = GetSingleScoreQuery(modeAndLevel, playerID);
                 //設定したクエリ(queryRequest)を非同期で実行、結果がresponseに格納される。
-                client.QueryAsync(highestScoreQuery, response =>
+                client.QueryAsync(singleQuery, response =>
                 {
                     //エラーがあればエラー内容を表示
                     if (response.Exception != null)
@@ -105,26 +101,37 @@ namespace AWS
                     var result = response.Response;//クエリの結果をresultに格納
 
                     DisplayScores record;
-                    if (result.Items.Count > 0)
+                    if (result.Items.Count == 1)
                     {
                         record = new DisplayScores()
                         {
-                            PlayerIDAndModeAndLevel = result.Items[0]["PlayerIDAndModeAndLevel"].S,
-                            Score = int.Parse(result.Items[0]["Score"].N)
+                            ModeAndLevel = result.Items[0]["ModeAndLevel"].S,
+                            Score = int.Parse(result.Items[0]["Score"].N),
+                            PlayerID = result.Items[0]["PlayerID"].S,
                         };
                         if (result.Items.Count > 1) Debug.LogError("単一のレコードを返すためのクエリに、複数のレコードが返ってきています。");
+                    }
+                    else if(result.Items.Count == 0)
+                    {
+                        record = new DisplayScores()
+                        {
+                            ModeAndLevel = "",
+                            Score = 0,
+                            PlayerID = playerID
+                        };
+                        Debug.LogWarning("初めてのPlayerIDAndModeAndLevelの更新です。");
                     }
                     else
                     {
                         record = new DisplayScores()
                         {
-                            PlayerIDAndModeAndLevel = "",
-                            Score = 0
+                            ModeAndLevel = "",
+                            Score = 0,
+                            PlayerID = playerID
                         };
-                        Debug.LogWarning("初めてのPlayerIDAndModeAndLevelの更新です。");
+                        Debug.LogError("レコード数が異常値です");
                     }
                     callback(record);
-
                 });
                 
 
@@ -138,12 +145,11 @@ namespace AWS
         //引数で指定されたモード・レベルに対応するレコードを削除する処理。スコアの更新の際、古いスコアを消去するために使う。
         Task DeleteOldScoreAsync(string modeAndLevel, int oldScore)
         {
-            string playerIDAndModeAndLevel = $"{playerID}_{modeAndLevel}";
             var source = new TaskCompletionSource<bool>();
             try
             {
                 Debug.Log($"{oldScore}がスコアのrecordを消去します");
-                var key = new DynamoDBDatas { PlayerIDAndModeAndLevel = playerIDAndModeAndLevel, Score = oldScore };
+                var key = new DynamoDBDatas { ModeAndLevel = modeAndLevel, Score = oldScore };
                 context.DeleteAsync(key, result =>
                 {
                     if (result.Exception == null)
@@ -169,11 +175,10 @@ namespace AWS
         //引数で指定されたモードとレベルに応じてDynamoDBから非同期で取得し、結果をコールバックとして戻す。
         public void GetTop10Scores(string modeAndLevel, Action<List<DisplayScores>> callback)
         {
-            string playerIDAndModeAndLevel = $"{playerID}_{modeAndLevel}";
             try
             {
                 //QueryAsyncを使用する際にはクエリの情報を持つクラスQueryRequestのインスタンスが必要で、ここでクエリの細かい設定を行う。
-                var top10ScoreQuery = GetRecordsQuery(playerIDAndModeAndLevel, false, 10);
+                var top10ScoreQuery = GetTop10RecordsQuery(modeAndLevel);
 
                 //設定したクエリ(queryRequest)を非同期で実行、結果がresponseに格納される。
                 client.QueryAsync(top10ScoreQuery, response =>
@@ -194,9 +199,10 @@ namespace AWS
                         records.Add(new DisplayScores
                         {
                             //「.N」や「.S」は内部のデータの型を表しており他にもバイナリ(B)、文字列リスト(SS)、数値リスト(NS)などがある。全て文字列として値が返ってくる点に注意。
-                            PlayerIDAndModeAndLevel = item["PlayerIDAndModeAndLevel"].S,
-                            Score = int.Parse(item["Score"].N)
-                        });
+                            ModeAndLevel = item["ModeAndLevel"].S,
+                            Score = int.Parse(item["Score"].N),
+                            PlayerID = item["PlayerID"].S
+                        }); ;
                     }
                     callback(records);
                 });
@@ -212,16 +218,20 @@ namespace AWS
         public class DynamoDBDatas
         {
             [DynamoDBHashKey]
-            public string PlayerIDAndModeAndLevel { get; set; }
+            public string ModeAndLevel { get; set; }
 
             [DynamoDBRangeKey]
             public int Score { get; set; }
+
+            [DynamoDBProperty]
+            public string PlayerID { get; set; }
         }
 
         public class DisplayScores
         {
-            public string PlayerIDAndModeAndLevel;
+            public string ModeAndLevel;
             public int Score;
+            public string PlayerID;
         }
 
         public bool WaitInitialize()
@@ -229,21 +239,38 @@ namespace AWS
             return context == null;
         }
 
-        //クエリを返すメソッド、引数でplayerIDAndModeAndLevel、昇順か降順か、取得するレコードの上限数を指定できる。
-        QueryRequest GetRecordsQuery(string playerIDAndModeAndLevel, bool isForward, int limitValue)
+        //クエリを返すメソッド、引数でModeAndLevelとIDからスコア一意に取得する。
+        QueryRequest GetSingleScoreQuery(string modeAndLevel, string playerID)
         {
             return new QueryRequest
             {
                 TableName = "PFT_PlayerScoreRanking",
-                KeyConditionExpression = "PlayerIDAndModeAndLevel = :v1", //PlayerIDAndModeAndLevelキーが:v1と一致するレコードを検索する。v1はプレースホルダーで、下の式で実際の値を割り当てている。
+                KeyConditionExpression = "ModeAndLevel = :v1", //ModeAndLevelキーが:v1と一致するレコードを検索する。v1はプレースホルダーで、下の式で実際の値を割り当てている。
+                FilterExpression = "PlayerID = :v2", //playerIDに対するフィルタ式、フィルタはクエリの実行後に適用されるため、大量のデータを扱う場合には注意
 
                 //ExpressionAttributeValuesは、プレースホルダー(Key)に実際の値(Value)を割り当てる役割を持っている。
                 //AttributeValueはDynamoDBの特別なオブジェクト型でDynamoDBで用いる様々な型のデータを扱えるようになっている。今回はString
                 ExpressionAttributeValues = new Dictionary<string, AttributeValue> {
-                    {":v1", new AttributeValue { S = playerIDAndModeAndLevel }}
+                {":v1", new AttributeValue { S = modeAndLevel }},
+                {":v2", new AttributeValue { S = playerID }}
                 },
-                ScanIndexForward = isForward, //降順で結果を表示
-                Limit = limitValue //レコード数の上限
+                Limit = 1 //レコード数の上限
+            };
+        }
+
+        //引数で受け取ったmodeAndLevelに応じたトップ10のレコードを取得するクエリ。
+        QueryRequest GetTop10RecordsQuery(string modeAndLevel)
+        {
+            return new QueryRequest
+            {
+                TableName = "PFT_PlayerScoreRanking",
+                KeyConditionExpression = "ModeAndLevel = :v1",
+                ExpressionAttributeValues = new Dictionary<string, AttributeValue> {
+                {":v1", new AttributeValue { S = modeAndLevel }},
+                },
+
+                ScanIndexForward = false,
+                Limit = 10
             };
         }
     }
