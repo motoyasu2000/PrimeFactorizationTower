851e3eb2ab0a48bedb824d9f7e08462a94facf41
motoyasu2000 <regenwurm770@gmail.com>
2024-03-22 03:25:09 +0900

    正常に世界ランキングが名前とともに表示されるように修正した。あとはゲームモードやローカルglobalを切り替えられるタブを作る。

--- Assets/Scripts/Appearance/UI/TitleScene/NameInputButton.cs
+++ Assets/Scripts/Appearance/UI/TitleScene/NameInputButton.cs
@@ -8,32 +8,46 @@ using System.Text.RegularExpressions;
 
 public class NameInputButton : MonoBehaviour
 {
-    TMP_InputField nameText;
-    TextMeshProUGUI errorText;
-    GameObject inputNameMenuBackGround;
+    TMP_InputField nameText; //名前を入力する欄
+    TextMeshProUGUI errorText; //エラー理由を表示するテキスト
+    GameObject inputNameMenuBackGround; //名前を入力する画面の全体。これを見えるようにすることで、名前入力の画面を表示させる。
 
+    //初期化
     private void Start()
     {
         nameText = GameObject.Find("NameInputField").GetComponent<TMP_InputField>();
         errorText = GameObject.Find("ErrorText").GetComponent<TextMeshProUGUI>();
         inputNameMenuBackGround = GameObject.Find("InputNameMenuBackGround");
     }
+
+    //名前の登録処理
     public void ConfirmName()
     {
-        string playerName = nameText.text.Trim();
-        Debug.Log(playerName);
-        if (playerName.Count() > 20)
+        string playerName = nameText.text.Trim(); //入力された名前の取得
+
+        if (CheckError(playerName)) return; //エラーがあれば以降の処理を行わない。
+
+        //名前を保存して、画面を非表示。
+        PlayerInfoManager.Ins.InputNameProcess(playerName);
+        inputNameMenuBackGround.SetActive(false);
+    }
+
+    //引数で受け取った文字列が利用可能な名前化を調査する。エラーがあればtrueを返す。
+    bool CheckError(string playerName)
+    {
+        //もし16文字より多ければエラー
+        if (playerName.Count() > 16)
         {
             errorText.text = "too long";
-            return;
+            return true;
         }
+        //もしアルファベット文字列以外が使われていたらエラー
         if (!IsAlphanumeric(playerName))
         {
             errorText.text = "alphanumeric characters only";
-            return;
+            return true;
         }
-        PlayerInfoManager.Ins.InputNameProcess(playerName);
-        inputNameMenuBackGround.SetActive(false);
+        return false;
     }
 
     //アルファベット文字列であればtrue
