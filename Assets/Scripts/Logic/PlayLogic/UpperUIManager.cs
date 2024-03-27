86832efaaadfcda7127cc103acabfa24b444d220
motoyasu2000 <regenwurm770@gmail.com>
2024-03-28 04:38:32 +0900

    gameManagerからUI操作に関わるものをUpperUIManagerに移動する2

--- Assets/Scripts/Logic/PlayLogic/UpperUIManager.cs
+++ Assets/Scripts/Logic/PlayLogic/UpperUIManager.cs
@@ -32,7 +32,7 @@ public class UpperUIManager : MonoBehaviour
         originNumberText.text = "";
     }
 
-    public void DisplayNumber(KindOfUI kindOfUI,string str)
+    public void ChangeDisplayText(KindOfUI kindOfUI,string str)
     {
         switch (kindOfUI)
         {
@@ -51,10 +51,23 @@ public class UpperUIManager : MonoBehaviour
         }
     }
 
-    public IEnumerator PrintConditionNumber(KindOfUI kindOfUI, string str, float seconds)
+    public void ChangeDisplayColor(KindOfUI kindOfUI, Color color)
     {
-        yield return new WaitForSeconds(seconds);
-        DisplayNumber(kindOfUI, str);
-        yield return null;
+        switch (kindOfUI)
+        {
+            case KindOfUI.Condition:
+                conditionNumberText.color = color;
+                break;
+            case KindOfUI.Origin:
+                originNumberText.color = color;
+                break;
+            case KindOfUI.NextOrigin:
+                nextOriginNumberText.color = color;
+                break;
+            default:
+                Debug.LogError("予期せぬKindOfUIが呼ばれました。");
+                break;
+        }
     }
+
 }
