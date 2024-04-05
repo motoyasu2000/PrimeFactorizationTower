ee9281fadf31512d43e193bdc7f1d32b9dda3e29
motoyasu2000 <regenwurm770@gmail.com>
2024-04-05 21:06:25 +0900

    与えられたenum型の数に応じた数だけスライダーを生成できるようにした。細かい名前の調整や、各スライダーの機能、マテリアルの選択機能はまだ。

--- Assets/Scripts/EnumManager.cs
+++ Assets/Scripts/EnumManager.cs
@@ -3,30 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Enum型を外から便利に扱うためのクラス
-public static class EnumManager
-{
-    //Enumの要素数を取得
-    public static int GetEnumCount<TEnum>() where TEnum : Enum
+namespace Common {
+    //Enum型を外から便利に扱うためのクラス
+    public static class EnumManager
     {
-        return Enum.GetValues(typeof(TEnum)).Length;
-    }
+        //Enumの要素数を取得
+        public static int GetEnumCount<TEnum>() where TEnum : Enum
+        {
+            return Enum.GetValues(typeof(TEnum)).Length;
+        }
 
-    //Enumの特定の要素のインデックスを取得
-    public static int GetEnumIndex<TEnum>(TEnum value) where TEnum : Enum
-    {
-        return Array.IndexOf(Enum.GetValues(typeof(TEnum)), value);
-    }
+        //Enumの特定の要素のインデックスを取得
+        public static int GetEnumIndex<TEnum>(TEnum value) where TEnum : Enum
+        {
+            return Array.IndexOf(Enum.GetValues(typeof(TEnum)), value);
+        }
 
-    //Enumの値から名前を取得
-    public static string GetEnumName<TEnum>(TEnum value) where TEnum : Enum
-    {
-        return Enum.GetName(typeof(TEnum), value);
-    }
+        //Enumの値から名前を取得
+        public static string GetEnumName<TEnum>(TEnum value) where TEnum : Enum
+        {
+            return Enum.GetName(typeof(TEnum), value);
+        }
 
-    //名前からEnumの値を取得
-    public static TEnum GetEnumValue<TEnum>(string name) where TEnum : Enum
-    {
-        return (TEnum)Enum.Parse(typeof(TEnum), name);
+        public static string[] GetEnumNames<TEnum>() where TEnum : Enum
+        {
+            return Enum.GetNames(typeof(TEnum));
+        }
+
+        //名前からEnumの値を取得
+        public static TEnum GetEnumValue<TEnum>(string name) where TEnum : Enum
+        {
+            return (TEnum)Enum.Parse(typeof(TEnum), name);
+        }
     }
 }
