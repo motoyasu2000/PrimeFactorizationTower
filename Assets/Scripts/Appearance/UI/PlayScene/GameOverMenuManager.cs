47f510e19cac54c6e81aa99abd7f3b68a717f3e5
motoyasu2000 <regenwurm770@gmail.com>
2024-06-10 21:22:44 +0900

    素因数分解のミスが直接的にゲームオーバーになるわけではないので、ゲームオーバーの理由を表示するメソッドを消去し、FellDownのみが表示されるようにした。

--- Assets/Scripts/Appearance/UI/PlayScene/GameOverMenuManager.cs
+++ Assets/Scripts/Appearance/UI/PlayScene/GameOverMenuManager.cs
@@ -9,21 +9,19 @@ namespace UI
     {
         int oldMaxScore;
         int newScore;
-        GameModeManager gameModeManager;
         GameOverManager gameOverManager;
-        GameObject UI_gameOverReason;
+        GameObject UI_TextFellDown;
         GameObject UI_TextGameOver;
         GameObject UI_nonUpdate; //スコアを更新しなかった場合のレコードの表示UI
         GameObject UI_updateRecord; //スコアを更新た場合のレコードの表示UI
         GameObject UI_winOrLose; //対戦モードで勝敗が決まった時に表示するUI
         void Awake()
         {
-            gameModeManager = GameModeManager.Ins;
             gameOverManager = GameObject.Find("GameOverManager").GetComponent<GameOverManager>();
 
             //非アクティブなのでtransform.Findで取得
             UI_TextGameOver = transform.Find("Text_GameOver").gameObject;
-            UI_gameOverReason = transform.Find("GameOverReason").gameObject;
+            UI_TextFellDown = transform.Find("TextFellDown").gameObject;
             UI_nonUpdate = transform.Find("Scores_NonUpdateRecord").gameObject;
             UI_updateRecord = transform.Find("Scores_UpdateRecord").gameObject;
             UI_winOrLose = transform.Find("WinOrLose").gameObject;
@@ -36,8 +34,8 @@ namespace UI
             else
             {
                 UI_TextGameOver.SetActive(true);
-                UI_gameOverReason.SetActive(true);
-                PrintGameOverReason();
+                UI_TextFellDown.SetActive(true);
+                UI_TextFellDown.SetActive(true);
                 DisplayScoreMenu();
             }
         }
@@ -47,24 +45,6 @@ namespace UI
             TextMeshProUGUI text = UI_winOrLose.GetComponent<TextMeshProUGUI>();
             if (TurnMangaer.GetPlayerNames_NowTurn() == PlayerInfoManager.Ins.PlayerName) text.text = "You Lose";
             else text.text = "You Win";
-        }
-
-        //ゲームオーバー時の画面にて、ゲームオーバーになった理由を表示するメソッド
-        void PrintGameOverReason()
-        {
-            TextMeshProUGUI reasonText = UI_gameOverReason.GetComponent<TextMeshProUGUI>();
-            switch (gameModeManager.NowGameMode)
-            {
-                //ゲームモードがPileUpであるとき、素因数分解を間違えた場合は、自分の選んだ数字で合成数を割った場合どうなるのかの表示、落下してしまった場合はFellDownと表示をする
-                case GameModeManager.GameMode.PileUp:
-                    if (gameOverManager.BlockNumberAtGameOver != 0) reasonText.text = 
-                            $"{gameOverManager.CompositeNumberAtGameOver} " +
-                            $"/ {gameOverManager.BlockNumberAtGameOver} " +
-                            $"= {gameOverManager.CompositeNumberAtGameOver / gameOverManager.BlockNumberAtGameOver}" +
-                            $"...{gameOverManager.CompositeNumberAtGameOver % gameOverManager.BlockNumberAtGameOver}";
-                    else reasonText.text = "Fell Down";
-                    break;
-            }
         }
 
         //ゲームオーバー時のスコアを表示させるメソッド 最高スコアを更新したか否かで異なるスコアの表示の仕方をする
