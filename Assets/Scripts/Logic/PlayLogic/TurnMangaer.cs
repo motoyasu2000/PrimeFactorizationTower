59534545e9cb4710e53c27482d37b275f86d863e
motoyasu2000 <regenwurm770@gmail.com>
2024-06-14 02:10:55 +0900

    可読性の向上

--- Assets/Scripts/Logic/PlayLogic/TurnMangaer.cs
+++ Assets/Scripts/Logic/PlayLogic/TurnMangaer.cs
@@ -5,34 +5,35 @@ using Common;
 //ターンを管理するクラス
 public static class TurnMangaer
 {
-    static int nowTurn = 0;
-    static int maxTurn = 0;
-    static List<string> names = new List<string>();
+    static int nowNamesIndex = 0; //今何番目の人か
+    static int maxNamesIndex = 0; //何番目の人までいるか
+    static List<string> names = new List<string>(); //すべての名前を保存するリスト
     static int totalTurn = 0;
 
     //プレイヤーの名前を設定する
     public static void SetPlayerNames(List<string> playerNames)
     {
         names = playerNames;
-        maxTurn = names.Count - 1;
+        maxNamesIndex = names.Count-1;
+        if (names.Count == 0) Debug.LogError("人が一人もいません。");
     }
 
     //現在のターンの人の名前を取得する
     public static string GetPlayerNames_NowTurn()
     {
-        return names[nowTurn];
+        return names[nowNamesIndex];
     }
 
     //以前のターンの人の名前を取得する
     public static string GetPlayerNames_BeforeTurn()
     {
-        if(nowTurn - 1 < 0)
+        if(nowNamesIndex - 1 < 0)
         {
-            return names[maxTurn];
+            return names[maxNamesIndex];
         }
         else
         {
-            return names[nowTurn - 1];
+            return names[nowNamesIndex - 1];
         }
     }
 
@@ -40,27 +41,27 @@ public static class TurnMangaer
     public static void ChangeNextTurn()
     {
         totalTurn++;
-        if (nowTurn < maxTurn)
+        if (nowNamesIndex < maxNamesIndex)
         {
-            nowTurn++;
+            nowNamesIndex++;
         }
-        else if(nowTurn == maxTurn)
+        else if(nowNamesIndex == maxNamesIndex)
         {
-            nowTurn = 0;
+            nowNamesIndex = 0;
         }
         else
         {
             Debug.LogError("turn数が異常値になりました。");
-            Debug.LogError($"現在のターン数{nowTurn} 上限ターン数{maxTurn}");
-            nowTurn = 0;
+            Debug.LogError($"現在のターン数{nowNamesIndex} 上限ターン数{maxNamesIndex}");
+            nowNamesIndex = 0;
         }
-        //Debug.Log(GetNowTurn());
+        Debug.Log(GetNowTurn());
     }
 
     //現在のターンを取得
     public static int GetNowTurn()
     {
-        return nowTurn;
+        return nowNamesIndex;
     }
 
     public static int GetTotalTurn()
